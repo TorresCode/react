a13165e09616b09d04d0a907804dea9151e6bdc5
Torrescode <torrescode01@gmail.com>
2024-06-20 19:04:41 +0100

    primary

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './App.css'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+const App = () => {
+  let message = 'Hello TC' 
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Studing React JS',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Studing Vue JS',
+      completed: true,
+    },
+    {
+      id: '3',
+      title: 'Studing Adonis JS',
+      completed: true,
+    }
+
+  ])
+
+  return <>
+    <div className='container'>
+      TorresCode Hello World!!!!
+    </div>
+  </>
+}
+
+export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Task.css'
+const Task = ( {task} ) => {
+    return ( 
+        <div className='task-container'>
+            <h1>{task.title}</h1>
+        </div>
+     )
+}
+
+export default Task;
